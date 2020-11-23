c4b84f6fd84497231132693f110415d9d01293a4
Evgeniy Lebedev <lebedev1537@gmail.com>
2020-11-23 18:52:59 +0400

    Migrate to mysql

--- server/src/main/resources/db/migration/V1.0.0__Init.sql
+++ server/src/main/resources/db/migration/V1.0.0__Init.sql
@@ -1,35 +1,35 @@
 create table attributes (
-	id integer identity primary key,
+	id integer unsigned auto_increment primary key,
 	name varchar(255),
 	type varchar(255)
 );
 
 create table tickets (
-	id integer identity primary key,
+	id integer unsigned auto_increment primary key,
 	number varchar(64),
 	comment varchar(1024),
 	tracked boolean
 );
 
 create table ticket_attributes (
-	id integer identity primary key,
-	ticket_id integer,
-	attribute_id integer,
+	id integer unsigned auto_increment primary key,
+	ticket_id integer unsigned,
+	attribute_id integer unsigned,
 	value varchar(255),
 	foreign key (ticket_id) references tickets(id),
 	foreign key (attribute_id) references attributes(id)
 );
 
 create table dashboards (
-	id integer identity primary key,
+	id integer unsigned auto_increment primary key,
 	name varchar(255),
 	query varchar(512)
 );
 
 create table dashboard_columns (
-	id integer identity primary key,
-	dashboard_id integer,
-	attribute_id integer,
+	id integer unsigned auto_increment primary key,
+	dashboard_id integer unsigned,
+	attribute_id integer unsigned,
 	foreign key (dashboard_id) references dashboards(id),
 	foreign key (attribute_id) references attributes(id)
 );
--- server/src/main/resources/db/migration/V1.0.3__Add_dashboard_column_fields.sql
+++ server/src/main/resources/db/migration/V1.0.3__Add_dashboard_column_fields.sql
@@ -1,5 +1,5 @@
 alter table dashboard_columns add column "ORDER" varchar(32);
 
-alter table dashboard_columns add column  position int;
+alter table dashboard_columns add column position integer;
 
-alter table dashboard_columns add column  width int;
+alter table dashboard_columns add column width integer;
